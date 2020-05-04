84be5a1dc018267c3144d2929d877319d7420f89
Moody Salem <moody.salem@gmail.com>
2020-05-04 18:01:57 -0400

    Factory address, fix checksum warning

--- src/constants.ts
+++ src/constants.ts
@@ -22,7 +22,7 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0xe2f197885abe8ec7c866cFf76605FD06d4576218'
+export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
 
 export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
 
--- test/pair.test.ts
+++ test/pair.test.ts
@@ -4,9 +4,9 @@ import { ChainId } from '../src/constants'
 describe('Pair', () => {
   describe('#getAddress', () => {
     it('returns the correct address', () => {
-      const usdc = new Token(ChainId.MAINNET, '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', 18, 'USDC', 'USD Coin')
-      const dai = new Token(ChainId.MAINNET, '0x6b175474e89094c44da98b954eedeac495271d0f', 18, 'DAI', 'DAI Stablecoin')
-      expect(Pair.getAddress(usdc, dai)).toEqual('0x910FA6BE0358EBA8C27d31EFFB40103A02938b66')
+      const usdc = new Token(ChainId.MAINNET, '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 18, 'USDC', 'USD Coin')
+      const dai = new Token(ChainId.MAINNET, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 18, 'DAI', 'DAI Stablecoin')
+      expect(Pair.getAddress(usdc, dai)).toEqual('0xAE461cA67B15dc8dc81CE7615e0320dA1A9aB8D5')
     })
   })
 })
