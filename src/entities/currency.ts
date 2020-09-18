042507a29a5abf03b20b5c47bb17c213e5192a10
Mulenga Bowa <mulengabowa53@gmail.com>
2020-09-19 00:50:55 +0200

    changed base currency

--- src/entities/currency.ts
+++ src/entities/currency.ts
@@ -16,7 +16,7 @@ export class Currency {
   /**
    * The only instance of the base class `Currency`.
    */
-  public static readonly ETHER: Currency = new Currency(18, 'ETH', 'Ether')
+  public static readonly ETHER: Currency = new Currency(18, 'FUSE', 'Fuse')
 
   /**
    * Constructs an instance of the base class `Currency`. The only instance of the base class `Currency` is `Currency.ETHER`.
