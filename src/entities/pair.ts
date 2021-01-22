ca841e0edd5b17e66e0865d854399157bb83fc02
Anxo Rodriguez <anxolin@gmail.com>
2021-01-22 11:34:43 +0100

    Make factory chain dependent

--- src/entities/pair.ts
+++ src/entities/pair.ts
@@ -5,18 +5,7 @@ import JSBI from 'jsbi'
 import { pack, keccak256 } from '@ethersproject/solidity'
 import { getCreate2Address } from '@ethersproject/address'
 
-import {
-  BigintIsh,
-  FACTORY_ADDRESS,
-  INIT_CODE_HASH,
-  MINIMUM_LIQUIDITY,
-  ZERO,
-  ONE,
-  FIVE,
-  _997,
-  _1000,
-  ChainId
-} from '../constants'
+import { BigintIsh, getFactoryParams, MINIMUM_LIQUIDITY, ZERO, ONE, FIVE, _997, _1000, ChainId } from '../constants'
 import { sqrt, parseBigintIsh } from '../utils'
 import { InsufficientReservesError, InsufficientInputAmountError } from '../errors'
 import { Token } from './token'
@@ -30,15 +19,17 @@ export class Pair {
   public static getAddress(tokenA: Token, tokenB: Token): string {
     const tokens = tokenA.sortsBefore(tokenB) ? [tokenA, tokenB] : [tokenB, tokenA] // does safety checks
 
+    const { factoryAddress, initCode } = getFactoryParams(tokenA.chainId)
+
     if (PAIR_ADDRESS_CACHE?.[tokens[0].address]?.[tokens[1].address] === undefined) {
       PAIR_ADDRESS_CACHE = {
         ...PAIR_ADDRESS_CACHE,
         [tokens[0].address]: {
           ...PAIR_ADDRESS_CACHE?.[tokens[0].address],
           [tokens[1].address]: getCreate2Address(
-            FACTORY_ADDRESS,
+            factoryAddress,
             keccak256(['bytes'], [pack(['address', 'address'], [tokens[0].address, tokens[1].address])]),
-            INIT_CODE_HASH
+            initCode
           )
         }
       }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,7 @@
 import JSBI from 'jsbi'
 export { JSBI }
 
-export {
-  BigintIsh,
-  Chain,
-  ChainId,
-  TradeType,
-  Rounding,
-  FACTORY_ADDRESS,
-  INIT_CODE_HASH,
-  MINIMUM_LIQUIDITY
-} from './constants'
+export { BigintIsh, Chain, ChainId, TradeType, Rounding, getFactoryParams, MINIMUM_LIQUIDITY } from './constants'
 
 export * from './errors'
 export * from './entities'
