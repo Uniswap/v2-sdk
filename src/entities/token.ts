5da4c9747fb1a0b39bf9458941be7aab77d9f4a2
Matthew Lilley <hello@matthewlilley.com>
2021-02-25 11:20:09 +0000

    chore: update wrapped native token map

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -63,11 +63,12 @@ export const WETH = {
   [ChainId.RINKEBY]: new Token(ChainId.RINKEBY, '0xc778417E063141139Fce010982780140Aa0cD5Ab', 18, 'WETH', 'Wrapped Ether'),
   [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH', 'Wrapped Ether'),
   [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH', 'Wrapped Ether'),
-  [ChainId.FANTOM]: new Token(ChainId.FANTOM, "0x91ea991bd52EE3C40EdA2509701d905e1Ee54074", 18, "WFTM", "Wrapped FTM"),
+  [ChainId.FANTOM]: new Token(ChainId.FANTOM, "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83", 18, "WFTM", "Wrapped FTM"),
   [ChainId.FANTOM_TESTNET]: new Token(ChainId.FANTOM_TESTNET, "0xf1277d1Ed8AD466beddF92ef448A132661956621", 18, "FTM", "Wrapped FTM"),
-  [ChainId.MATIC]: new Token(ChainId.MATIC, "0x0000000000000000000000000000000000001010", 18, "WMATIC", "Wrapped Matic"),
-  [ChainId.MATIC_TESTNET]: new Token(ChainId.MATIC_TESTNET, "0x0000000000000000000000000000000000001010", 18, "WMATIC", "Wrapped Matic"),
+  [ChainId.MATIC]: new Token(ChainId.MATIC, "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270", 18, "WMATIC", "Wrapped Matic"),
+  [ChainId.MATIC_TESTNET]: new Token(ChainId.MATIC_TESTNET, "0x5B67676a984807a212b1c59eBFc9B3568a474F0a", 18, "WMATIC", "Wrapped Matic"),
   [ChainId.XDAI]: new Token(ChainId.XDAI, "0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d", 18, "WXDAI", "Wrapped xDai"),
   [ChainId.BSC]: new Token(ChainId.BSC, "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c", 18, "WBNB", "Wrapped BNB"),
   [ChainId.BSC_TESTNET]: new Token(ChainId.BSC_TESTNET, "0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd", 18, "WBNB", "Wrapped BNB"),
+  [ChainId.ARBITRUM]: new Token(ChainId.ARBITRUM, "0xf8456e5e6A225C2C1D74D8C9a4cB2B1d5dc1153b", 18, "WETH", "Wrapped Ether")
 }
