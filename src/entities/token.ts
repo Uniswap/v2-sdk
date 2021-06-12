e0dead99eafec60e883bb46b7c66485076580161
Matthew Lilley <hello@matthewlilley.com>
2021-06-12 05:26:34 +0100

    chore: remove comment

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -102,8 +102,6 @@ export const WETH9: { [chainId: number]: Token } = {
   )
 }
 
-// In reality this is a map of the wrapped version of the native token for a given network.
-// TODO: Rename to WNATIVE for sanity
 export const WNATIVE = {
   [ChainId.MAINNET]: WETH9[ChainId.MAINNET],
   [ChainId.ROPSTEN]: WETH9[ChainId.ROPSTEN],
