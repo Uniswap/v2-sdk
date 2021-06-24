6c4d57b5eac4340159bb85a6749367326e628966
ilya <bretnwood@gmail.com>
2021-06-24 17:51:32 +0300

    MultiRouterSingle is separated to 3 routers

--- src/MultiRouter.ts
+++ src/MultiRouter.ts
@@ -16,39 +16,57 @@ interface Pool {
     fee: number;
 }
 
-enum Topology {
-    Single = "Single",
-    Parallel = "Parallel",
-    Serial = "Serial"
-}
-
 abstract class MultiRouter {
-    topology: Topology;
-
-    constructor(_top: Topology) {
-        this.topology = _top;
-    }
-
     abstract calcOutByIn(amountIn: number): number;
     abstract calcPrice(amountIn: number): number;
     abstract calcInputByPrice(price: number, hint: number): number;
 }
 
-class MultiRouterSingle extends MultiRouter {
+class MultiRouterConstantProduct extends MultiRouter {
     pool: Pool;
-    HybridD?: number;
 
     constructor(_pool: Pool) {
-        super(Topology.Single);
+        super();
+        console.assert(_pool.type == PoolType.ConstantProduct, 
+            "Wrong pool type " + _pool.type + " != " + PoolType.ConstantProduct);
         this.pool = _pool;
     }
 
-    getConstantMeanParams(): [number, number] {
+    calcOutByIn(amountIn: number): number {
+        const pool = this.pool;
+        return pool.reserve1*amountIn/(pool.reserve0/(1-pool.fee) + amountIn);
+    }
+
+    calcPrice(amountIn: number): number {
+        const pool = this.pool;
+        const x = pool.reserve0/(1-pool.fee);
+        return pool.reserve1*x/(x+amountIn)/(x+amountIn);
+    }
+
+    calcInputByPrice(price: number): number {
+        const pool = this.pool;
+        const x = pool.reserve0/(1-pool.fee);
+        const res =  Math.sqrt(pool.reserve1*x*price) - x;
+        return res;
+    }
+}
+
+class MultiRouterConstantMean extends MultiRouter {
+    pool: Pool;
+
+    constructor(_pool: Pool) {
+        super();
+        console.assert(_pool.type == PoolType.ConstantMean, 
+            "Wrong pool type " + _pool.type + " != " + PoolType.ConstantMean);
+        this.pool = _pool;
+    }
+
+    getWeights(): [number, number] {
         const arr = new Uint8Array(this.pool.data);
         return [arr[0], 100-arr[0]];
     }
 
-    setConstantMeanDataFromParams(weight0: number, weight1: number) {
+    setWeights(weight0: number, weight1: number) {
         console.assert(weight0+weight1 == 100, 'Weight wrong')
         const data = new ArrayBuffer(16);
         const arr = new Uint8Array(data);
@@ -56,12 +74,48 @@ class MultiRouterSingle extends MultiRouter {
         this.pool.data = data;
     }
 
-    getHybridParams(): number {
+    calcOutByIn(amountIn: number): number {
+        const pool = this.pool;
+        const [weight0, weight1] = this.getWeights();
+        const weightRatio = weight0/weight1;
+        const actualIn = amountIn*(1-pool.fee);
+        return pool.reserve1*(1-Math.pow(pool.reserve0/(pool.reserve0+actualIn), weightRatio));
+    }
+
+    calcPrice(amountIn: number): number {
+        const pool = this.pool;
+        const [weight0, weight1] = this.getWeights();
+        const weightRatio = weight0/weight1;
+        const x = pool.reserve0+amountIn*(1-pool.fee);
+        return pool.reserve1*weightRatio*(1-pool.fee)*Math.pow(pool.reserve0/x, weightRatio)/x;
+    }
+
+    calcInputByPrice(price: number): number {
+        const pool = this.pool;
+        const w = this.getWeights();
+        const weightRatio = w[0]/w[1];
+        const t = pool.reserve1*price*weightRatio*(1-pool.fee)*Math.pow(pool.reserve0, weightRatio);
+        return (Math.pow(t, 1/(weightRatio+1)) - pool.reserve0)/(1-pool.fee);
+    }
+}
+
+class MultiRouterHybrid extends MultiRouter {
+    pool: Pool;
+    HybridD?: number;
+
+    constructor(_pool: Pool) {
+        super();
+        console.assert(_pool.type == PoolType.Hybrid, 
+            "Wrong pool type " + _pool.type + " != " + PoolType.Hybrid);
+        this.pool = _pool;
+    }
+
+    getA(): number {
         const arr = new Int32Array(this.pool.data);
         return arr[0];
     }
 
-    setHybridDataFromParams(A: number) {
+    setA(A: number) {
         const data = new ArrayBuffer(16);
         const arr = new Int32Array(data);
         arr[0] = A;
@@ -69,7 +123,7 @@ class MultiRouterSingle extends MultiRouter {
         this.HybridD = undefined;
     }
 
-    computeHybridLiquidity(): number {
+    computeLiquidity(): number {
         const pool = this.pool;
         if (this.HybridD != undefined)
             return this.HybridD;
@@ -80,7 +134,7 @@ class MultiRouterSingle extends MultiRouter {
             return 0;
         }
 
-        const A = this.getHybridParams();
+        const A = this.getA();
         const nA = A * 2;
 
         let prevD;
@@ -97,163 +151,36 @@ class MultiRouterSingle extends MultiRouter {
         return D;
     }
 
-    computeHybridY(x: number): number {
-        const D = this.computeHybridLiquidity();
-        const A = this.getHybridParams();
+    computeY(x: number): number {
+        const D = this.computeLiquidity();
+        const A = this.getA();
         return calcSquareEquation(16*A*x, 16*A*x*x + 4*D*x - 16*A*D*x, -D*D*D)[1];
-    }
-
-    calcInputByPriceConstantMean(price:number): number {
-        const pool = this.pool;
-        const w = this.getConstantMeanParams();
-        const weightRatio = w[0]/w[1];
-        const t = pool.reserve1*price*weightRatio*(1-pool.fee)*Math.pow(pool.reserve0, weightRatio);
-        return (Math.pow(t, 1/(weightRatio+1)) - pool.reserve0)/(1-pool.fee);
     }
 
     calcOutByIn(amountIn: number): number {
         const pool = this.pool;
-        switch(pool.type) {
-            case PoolType.ConstantProduct: {
-                return pool.reserve1*amountIn/(pool.reserve0/(1-pool.fee) + amountIn);
-            } 
-            case PoolType.ConstantMean: {
-                const [weight0, weight1] = this.getConstantMeanParams();
-                const weightRatio = weight0/weight1;
-                const actualIn = amountIn*(1-pool.fee);
-                return pool.reserve1*(1-Math.pow(pool.reserve0/(pool.reserve0+actualIn), weightRatio));
-            } 
-            case PoolType.Hybrid: {
-                const xNew = pool.reserve0 + amountIn;
-                const yNew = this.computeHybridY(xNew);
-                const dy = (pool.reserve1 - yNew)*(1-pool.fee); // TODO: Why other pools take fees at the beginning, and this one - at the end?
-                return dy;
-            }
-        }
-        console.assert("Unknown Pool type: " + pool.type);
+        const xNew = pool.reserve0 + amountIn;
+        const yNew = this.computeY(xNew);
+        const dy = (pool.reserve1 - yNew)*(1-pool.fee); // TODO: Why other pools take fees at the beginning, and this one - at the end?
+        return dy;
     }
 
     calcPrice(amountIn: number): number {
         const pool = this.pool;
-        switch(pool.type) {
-            case PoolType.ConstantProduct: {
-                const x = pool.reserve0/(1-pool.fee);
-                return pool.reserve1*x/(x+amountIn)/(x+amountIn);
-            } 
-            case PoolType.ConstantMean: {
-                const [weight0, weight1] = this.getConstantMeanParams();
-                const weightRatio = weight0/weight1;
-                const x = pool.reserve0+amountIn*(1-pool.fee);
-                return pool.reserve1*weightRatio*(1-pool.fee)*Math.pow(pool.reserve0/x, weightRatio)/x;
-            } 
-            case PoolType.Hybrid: {
-                const D = this.computeHybridLiquidity();
-                const A = this.getHybridParams();
-                const x = pool.reserve0 + amountIn;
-                const b = 4*A*x + D - 4*A*D;
-                const ac4 = D*D*D/x;
-                const Ds = Math.sqrt(b*b + 4*A*ac4);
-                const res = (0.5 - (2*b-ac4/x)/Ds/4)*(1-pool.fee);
-                return res;
-            }
-        }
-        console.assert("Unknown Pool type: " + pool.type);
+        const D = this.computeLiquidity();
+        const A = this.getA();
+        const x = pool.reserve0 + amountIn;
+        const b = 4*A*x + D - 4*A*D;
+        const ac4 = D*D*D/x;
+        const Ds = Math.sqrt(b*b + 4*A*ac4);
+        const res = (0.5 - (2*b-ac4/x)/Ds/4)*(1-pool.fee);
+        return res;
     }
 
     calcInputByPrice(price: number, hint = 1): number {
-        const pool = this.pool;
-        switch(pool.type) {
-            case PoolType.ConstantProduct: {
-                const x = pool.reserve0/(1-pool.fee);
-                const res =  Math.sqrt(pool.reserve1*x*price) - x;
-                return res;
-            } 
-            case PoolType.ConstantMean: {
-                const res = this.calcInputByPriceConstantMean(price);
-                return res;
-            } 
-            case PoolType.Hybrid: {
-                return revertPositive( (x:number) => 1/this.calcPrice(x), price, hint);
-            }
-        }
-        console.assert("Unknown Pool type: " + pool.type);
+        return revertPositive( (x:number) => 1/this.calcPrice(x), price, hint);
     }
 }
-
-// class MultiRouterParallel extends MultiRouter {
-//     subRouters: MultiRouter[];
-
-//     constructor(_sub: MultiRouter[]) {
-//         super(Topology.Parallel);
-//         this.subRouters = _sub;
-//     }
-        
-//     findBestDistributionWithoutTransactionCost(
-//         amountIn: number,
-//         subRouters: MultiRouter[]       // TODO: maybe use initial distribution?
-//     ): [number, number[]] {
-
-//         if (subRouters.length == 1) {
-//             return [subRouters[0].calcOutByIn(amountIn), [1]];
-//         }
-
-//         let distr = subRouters.map(p => Math.max(p.calcOutByIn(amountIn/subRouters.length), 0));
-        
-//         for(let i = 0; i < 5; ++i) {
-//             const sum = distr.reduce((a, b) => a+b, 0);
-//             console.assert(sum > 0, "Error 508 " + sum);
-            
-//             const prices = distr.map((d, j) => 1/calcPrice(pools[j], amountIn*d/sum))
-//             const pr = prices.reduce((a, b) => Math.max(a, b), 0);
-            
-//             distr = pools.map((p, i) => calcInputByPrice(p, pr, distr[i]));        
-//         }
-
-//         const sum = distr.reduce((a, b) => a + b, 0);
-//         distr = distr.map(d => d/sum);
-//         const out = distr.map((p, i) => calcOutByIn(pools[i], p*amountIn)).reduce((a, b) => a+b, 0);
-
-//         return [out, distr];
-//     }
-
-//     findBestDistribution(
-//         amountIn: number,
-//         pools: Pool[],
-//         tokenOutPriceBase: number,
-//         gasPriceGWeiBase: number
-//     ): Route  | [number, number[][]]{
-//         const legPriceInTokenOut = LegGasConsuming*gasPriceGWeiBase*1e-9/tokenOutPriceBase;
-
-//         let [bestOut, distr] = findBestDistributionWithoutTransactionCost(amountIn, pools);
-//         bestOut -= legPriceInTokenOut*pools.length;
-//         let bestGroup = distr.map((d, i) => [i, d]).sort((a,b) => b[1] - a[1]);
-        
-//         let flagDown = false;
-//         const poolsSorted = bestGroup.map(a => pools[a[0]]);    
-//         for (let i = pools.length-1; i >= 1; --i) {
-//             const group = poolsSorted.slice(0, i);
-//             let [out, distr] = findBestDistributionWithoutTransactionCost(amountIn, group);
-//             out -= legPriceInTokenOut*i;
-            
-//             if (out > bestOut) {
-//                 console.assert(flagDown == false, "408 flagDown at " + amountIn);
-//                 bestOut = out;
-//                 bestGroup = distr.map((d, i) => [bestGroup[i][0], d]);
-//             } else {
-//                 flagDown = true;
-//             // break;            // TODO: uncomment for speed up ???
-//             }
-//         }
-            
-//         const checkedOut = calcOut(amountIn, pools, bestGroup, tokenOutPriceBase, gasPriceGWeiBase);
-//         return [checkedOut, bestGroup];
-//     }
-
-
-//     calcOutByIn(amountIn: number): number {
-
-//     }
-// }
 
 
 function calcSquareEquation(a:number, b:number, c:number): [number, number] {
