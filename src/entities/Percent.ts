6179584358ccfa3cd041c176b230b64ca1f4fd19
Matthew Lilley <hello@matthewlilley.com>
2021-06-29 03:02:01 +0100

    refactor: percent

--- src/entities/Percent.ts
+++ src/entities/Percent.ts
@@ -1,20 +1,48 @@
+import { BigintIsh } from '../types'
 import { Fraction } from './Fraction'
+import JSBI from 'jsbi'
 import { Rounding } from '../enums'
-import { _100 } from '../constants'
 
-const _100_PERCENT = new Fraction(_100)
+const ONE_HUNDRED = new Fraction(JSBI.BigInt(100))
+
+/**
+ * Converts a fraction to a percent
+ * @param fraction the fraction to convert
+ */
+function toPercent(fraction: Fraction): Percent {
+  return new Percent(fraction.numerator, fraction.denominator)
+}
 
 export class Percent extends Fraction {
+  /**
+   * This boolean prevents a fraction from being interpreted as a Percent
+   */
+  public readonly isPercent: true = true
+
+  add(other: Fraction | BigintIsh): Percent {
+    return toPercent(super.add(other))
+  }
+
+  subtract(other: Fraction | BigintIsh): Percent {
+    return toPercent(super.subtract(other))
+  }
+
+  multiply(other: Fraction | BigintIsh): Percent {
+    return toPercent(super.multiply(other))
+  }
+
+  divide(other: Fraction | BigintIsh): Percent {
+    return toPercent(super.divide(other))
+  }
+
   public toSignificant(
     significantDigits: number = 5,
     format?: object,
     rounding?: Rounding
   ): string {
-    return this.multiply(_100_PERCENT).toSignificant(
-      significantDigits,
-      format,
-      rounding
-    )
+    return super
+      .multiply(ONE_HUNDRED)
+      .toSignificant(significantDigits, format, rounding)
   }
 
   public toFixed(
@@ -22,6 +50,6 @@ export class Percent extends Fraction {
     format?: object,
     rounding?: Rounding
   ): string {
-    return this.multiply(_100_PERCENT).toFixed(decimalPlaces, format, rounding)
+    return super.multiply(ONE_HUNDRED).toFixed(decimalPlaces, format, rounding)
   }
 }
