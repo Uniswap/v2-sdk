c7cd99e7b420e4576434c54e387584164618d711
Matthew Lilley <hello@matthewlilley.com>
2021-07-19 19:28:43 +0100

    fix: network typo for WETH9 on kovan

--- src/entities/Token.ts
+++ src/entities/Token.ts
@@ -104,8 +104,8 @@ export const WETH9: { [chainId: number]: Token } = {
     'WETH9',
     'Wrapped Ether'
   ),
-  [ChainId.RINKEBY]: new Token(
-    ChainId.RINKEBY,
+  [ChainId.KOVAN]: new Token(
+    ChainId.KOVAN,
     '0xd0A1E359811322d97991E03f863a0C30C2cF029C',
     18,
     'WETH9',
