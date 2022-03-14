cd2d913e26e9e1a1c6ab1f39cb96a1aba4190350
cuonghx-gutech <cuonghx@gu-tech.com>
2022-03-14 13:30:14 +0700

    #update: gitignore

--- dist/constants.d.ts
+++
@@ -1,9 +0,0 @@
-import JSBI from 'jsbi';
-export declare const FACTORY_ADDRESS = "0xe11890E26A5035E6a72d249bB7D24deC14b1cD34";
-export declare const INIT_CODE_HASH = "0x3f013e20fe2f0b977a92a020c887c9e0ae068d5462d108951b1b192096b78072";
-export declare const MINIMUM_LIQUIDITY: JSBI;
-export declare const ZERO: JSBI;
-export declare const ONE: JSBI;
-export declare const FIVE: JSBI;
-export declare const _997: JSBI;
-export declare const _1000: JSBI;
