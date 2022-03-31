709193d691a736a73ed39763fc1fcd74ff670c86
oceandev999 <oceandev999@gmail.com>
2022-04-01 04:12:59 +0800

    Update SDK

--- README.md
+++
@@ -1,9 +0,0 @@
-# InterCroneswap V2 SDK
-
-[![Unit Tests](https://github.com/InterCroneworldOrg/v2-sdk/workflows/Unit%20Tests/badge.svg)](https://github.com/InterCroneworldOrg/v2-sdk/actions?query=workflow%3A%22Unit+Tests%22)
-[![Lint](https://github.com/InterCroneworldOrg/v2-sdk/workflows/Lint/badge.svg)](https://github.com/InterCroneworldOrg/v2-sdk/actions?query=workflow%3ALint)
-[![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
-[![npm version](https://img.shields.io/npm/v/@intercroneswap/v2-sdk/latest.svg)](https://www.npmjs.com/package/@intercroneswap/v2-sdk/v/latest)
-[![npm bundle size (scoped version)](https://img.shields.io/bundlephobia/minzip/@intercroneswap/v2-sdk/latest.svg)](https://bundlephobia.com/result?p=@intercroneswap/v2-sdk@latest)
-
-In-depth documentation on this SDK is available at [intercroneswap.org](https://intercroneswap.org/docs/v2/SDK/getting-started/).
---
+++ src/sdk/dist/constants.d.ts
@@ -0,0 +1,43 @@
+import JSBI from 'jsbi';
+export declare type BigintIsh = JSBI | number | string;
+export declare enum ChainId {
+    MAINNET = 56,
+    TESTNET = 97
+}
+export declare enum TradeType {
+    EXACT_INPUT = 0,
+    EXACT_OUTPUT = 1
+}
+export declare enum Rounding {
+    ROUND_DOWN = 0,
+    ROUND_HALF_UP = 1,
+    ROUND_UP = 2
+}
+export declare const FACTORY_ADDRESS = "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73";
+export declare const FACTORY_ADDRESS_MAP: {
+    56: string;
+    97: string;
+};
+export declare const INIT_CODE_HASH = "0x00fb7f630766e6a796048ea87d01acd3068e8ff67d078148a3fa3f4a84f69bd5";
+export declare const INIT_CODE_HASH_MAP: {
+    56: string;
+    97: string;
+};
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const TWO: JSBI;
+export declare const THREE: JSBI;
+export declare const FIVE: JSBI;
+export declare const TEN: JSBI;
+export declare const _100: JSBI;
+export declare const FEES_NUMERATOR: JSBI;
+export declare const FEES_DENOMINATOR: JSBI;
+export declare enum SolidityType {
+    uint8 = "uint8",
+    uint256 = "uint256"
+}
+export declare const SOLIDITY_TYPE_MAXIMA: {
+    uint8: JSBI;
+    uint256: JSBI;
+};
