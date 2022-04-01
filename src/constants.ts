0ec85220deb22bea5fb8e8320d5ec92e5efd4f54
oceandev999 <oceandev999@gmail.com>
2022-04-01 18:33:23 +0800

    Update SDK

---
+++ src/constants.ts
@@ -0,0 +1,57 @@
+import JSBI from 'jsbi'
+
+// exports for external consumption
+export type BigintIsh = JSBI | number | string
+
+export enum ChainId {
+  MAINNET = 56,
+  TESTNET = 97
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+export const FACTORY_ADDRESS = '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73'
+
+export const FACTORY_ADDRESS_MAP = {
+  [ChainId.MAINNET]: FACTORY_ADDRESS,
+  [ChainId.TESTNET]: '0xf3a0a9095b8b05d46f3ee8fe1d66c259de4cb364'
+}
+
+export const INIT_CODE_HASH = '0x00fb7f630766e6a796048ea87d01acd3068e8ff67d078148a3fa3f4a84f69bd5'
+
+export const INIT_CODE_HASH_MAP = {
+  [ChainId.MAINNET]: INIT_CODE_HASH,
+  [ChainId.TESTNET]: '0x21a1c45127cc5500cc626d621f5483247142398cc2fb2fe83a73fd9b20ea79c7'
+}
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
+export const FEES_NUMERATOR = JSBI.BigInt(9970)
+export const FEES_DENOMINATOR = JSBI.BigInt(10000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import JSBI from 'jsbi'
+export { JSBI }
+
+export {
+  BigintIsh,
+  ChainId,
+  TradeType,
+  Rounding,
+  FACTORY_ADDRESS,
+  FACTORY_ADDRESS_MAP,
+  INIT_CODE_HASH,
+  INIT_CODE_HASH_MAP,
+  MINIMUM_LIQUIDITY
+} from './constants'
+
+export * from './errors'
+export * from './entities'
+export * from './router'
+export * from './fetcher'
