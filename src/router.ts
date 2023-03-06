33782cb233e2366f3a5f8ee02ddef94a6478a63c
A.L <xenide@users.noreply.github.com>
2023-03-07 00:05:54 +0800

    feat: ERC-2616 permit
    * fix: rename SwapParameters into MethodParameters
    * Fix code style issues with Prettier
    * chore: import permit files from uni verbatim
    * fix: import paths and json
    * fix: update calldata type to only be `string`
    * feat: ERC-2616 permit works
    * Fix code style issues with Prettier
    * feat: permit for removeLiquidity
    * Fix code style issues with Prettier
    * chore: upgrade version to 0.1.0
    ---------
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,5 @@
 import invariant from 'tiny-invariant'
-import {
-  Token,
-  Currency,
-  CurrencyAmount,
-  Percent,
-  TradeType,
-  validateAndParseAddress,
-  BigintIsh
-} from '@reservoir-labs/sdk-core'
+import { Token, Currency, CurrencyAmount, Percent, TradeType, validateAndParseAddress } from '@reservoir-labs/sdk-core'
 import { Pair, Trade } from './entities'
 import { Multicall } from './multicall'
 import { Payments } from './payments'
@@ -16,6 +8,7 @@ import { Interface } from '@ethersproject/abi'
 import IReservoirRouter from './abis/IReservoirRouter.json'
 import { ROUTER_ADDRESS } from './constants'
 import { calculateSlippageAmount } from './utils/math'
+import { PermitOptions, SelfPermit } from './selfPermit'
 
 /**
  * Options for producing the arguments to send call to the router.
@@ -39,11 +32,11 @@ export interface TradeOptions {
 /**
  * The parameters to use in the call to the Uniswap V2 Router to execute a trade.
  */
-export interface SwapParameters {
+export interface MethodParameters {
   /**
    * The arguments to pass to the method, all hex encoded.
    */
-  calldata: (string | string[] | number[])[] | string
+  calldata: string
   /**
    * The amount of wei to send in hex.
    */
@@ -69,14 +62,25 @@ export abstract class Router {
    * Produces the on-chain method name to call and the hex encoded parameters to pass as arguments for a given trade.
    * @param trade to produce call parameters for
    * @param options options for the call parameters
+   * @param permit ERC-2616 or DAI style permit signature signed by the wallet for the input token, if applicable
    */
-  public static swapCallParameters(trade: Trade<Currency, Currency, TradeType>, options: TradeOptions): SwapParameters {
+  public static swapCallParameters(
+    trade: Trade<Currency, Currency, TradeType>,
+    options: TradeOptions,
+    permit?: PermitOptions
+  ): MethodParameters {
     const etherIn = trade.inputAmount.currency.isNative
     const etherOut = trade.outputAmount.currency.isNative
     // the router does not support both ether in and out
     invariant(!(etherIn && etherOut), 'ETHER_IN_OUT')
 
     const calldatas: string[] = []
+
+    // the permit is assumed to be for the input token
+    if (permit) {
+      const encodedPermit = SelfPermit.encodePermit(trade.inputAmount.wrapped.currency, permit)
+      calldatas.push(encodedPermit)
+    }
 
     const to: string = etherOut ? ROUTER_ADDRESS : validateAndParseAddress(options.recipient)
     const amountIn: string = toHex(trade.maximumAmountIn(options.allowedSlippage))
@@ -115,7 +119,6 @@ export abstract class Router {
     // encodeMulticall checks if the array is larger than 1
     // so if no native tokens are involved multicall would not be used
     const calldata = Multicall.encodeMulticall(calldatas)
-    console.log('final calldata', calldata)
 
     // the difference between a nativeIn swap vs a wrapped native token swap is that
     // the nativeIn swap would have value attached to it, but the wrapped one would not have value
@@ -126,15 +129,24 @@ export abstract class Router {
   }
 
   public static addLiquidityParameters(
-    tokenAmountA: CurrencyAmount<any>,
-    tokenAmountB: CurrencyAmount<any>,
+    tokenAmountA: CurrencyAmount<Currency>,
+    tokenAmountB: CurrencyAmount<Currency>,
     curveId: number,
-    options: TradeOptions
-  ): SwapParameters {
+    options: TradeOptions,
+    tokenAPermit?: PermitOptions,
+    tokenBPermit?: PermitOptions
+  ): MethodParameters {
     invariant(!tokenAmountA.currency.equals(tokenAmountB.currency), 'ATTEMPTING_TO_ADD_LIQ_FOR_SAME_TOKEN')
     invariant(curveId === 0 || curveId === 1, 'INVALID_CURVE_ID')
     const etherIn = tokenAmountA.currency.isNative || tokenAmountB.currency.isNative
     const calldatas: string[] = []
+
+    if (tokenAPermit) {
+      calldatas.push(SelfPermit.encodePermit(tokenAmountA.wrapped.currency, tokenAPermit))
+    }
+    if (tokenBPermit) {
+      calldatas.push(SelfPermit.encodePermit(tokenAmountB.wrapped.currency, tokenBPermit))
+    }
 
     const methodName = 'addLiquidity'
     const args = [
@@ -171,13 +183,18 @@ export abstract class Router {
     tokenAmountA: CurrencyAmount<Currency>,
     tokenAmountB: CurrencyAmount<Currency>,
     curveId: number,
-    liquidityAmount: BigintIsh,
-    options: TradeOptions
-  ): SwapParameters {
+    liquidityAmount: CurrencyAmount<Token>,
+    options: TradeOptions,
+    liquidityTokenPermit?: PermitOptions
+  ): MethodParameters {
     invariant(!tokenAmountA.currency.equals(tokenAmountB.currency), 'ATTEMPTING_TO_REMOVE_LIQ_FOR_SAME_TOKEN')
     const etherOut = tokenAmountA.currency.isNative || tokenAmountB.currency.isNative
     const validatedRecipient = validateAndParseAddress(options.recipient)
     const calldatas: string[] = []
+
+    if (liquidityTokenPermit) {
+      calldatas.push(SelfPermit.encodePermit(liquidityAmount.currency, liquidityTokenPermit))
+    }
 
     const methodName = 'removeLiquidity'
     const to = etherOut ? ROUTER_ADDRESS : validatedRecipient
@@ -187,7 +204,7 @@ export abstract class Router {
       tokenAmountA.wrapped.currency.address,
       tokenAmountB.wrapped.currency.address,
       curveId,
-      liquidityAmount.toString(),
+      liquidityAmount.quotient.toString(),
       tokenAMinimumAmount.toString(),
       tokenBMinimumAmount.toString(),
       to
