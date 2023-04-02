abf22676ec789f1458eb4ced331f17819130c65e
Lint Action <lint-action@samuelmeuli.com>
2023-04-02 16:12:41 +0000

    Fix code style issues with Prettier

--- src/lib/stableMath.test.ts
+++ src/lib/stableMath.test.ts
@@ -1,15 +1,14 @@
-import {calculateStableSpotPrice} from "./stableMath";
-import {DEFAULT_AMPLIFICATION_COEFFICIENT_PRECISE} from "../constants";
-
+import { calculateStableSpotPrice } from './stableMath'
+import { DEFAULT_AMPLIFICATION_COEFFICIENT_PRECISE } from '../constants'
 
 describe('stableMath', () => {
-    it('calculateStableSpotPrice', () => {
-        const scaledReserve0 = '103219283019283019283'
-        const scaledReserve1 = '5003210341820391823093'
-        const amplificationCoefficient = DEFAULT_AMPLIFICATION_COEFFICIENT_PRECISE
+  it('calculateStableSpotPrice', () => {
+    const scaledReserve0 = '103219283019283019283'
+    const scaledReserve1 = '5003210341820391823093'
+    const amplificationCoefficient = DEFAULT_AMPLIFICATION_COEFFICIENT_PRECISE
 
-        const result = calculateStableSpotPrice(scaledReserve0, scaledReserve1, amplificationCoefficient.toString())
+    const result = calculateStableSpotPrice(scaledReserve0, scaledReserve1, amplificationCoefficient.toString())
 
-        expect(result.toString()).toEqual('1.2987033193626902185')
-    })
+    expect(result.toString()).toEqual('1.2987033193626902185')
+  })
 })
