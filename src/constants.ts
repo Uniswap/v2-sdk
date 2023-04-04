250c03985523238b11383983c8b2259e7dc36635
A.L <xenide@users.noreply.github.com>
2023-04-04 20:08:53 +0800

    fix: update router address for testnet

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,10 @@ import JSBI from 'jsbi'
 // TODO: update this with the standardized CREATE2 address that will be the same across all chains
 // Currently it only works for the wallet with the custom mnemonic given to anvil
 export const FACTORY_ADDRESS = '0xCae997a6f253814441B878868fd6DBB32a52816f'
-export const ROUTER_ADDRESS = '0x7f05c63dc7ca3f99f2d3409f0017c28058c42b27'
+
+// This address is the product of using create2 with testnet WAVAX's contract address
+const TESTNET_ROUTER_ADDRESS = '0xd627FdC984a249E9b5F2df263A37368f4e459726'
+export const ROUTER_ADDRESS = TESTNET_ROUTER_ADDRESS
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 export const FEE_ACCURACY = JSBI.BigInt(1_000_000) // 100%
 
