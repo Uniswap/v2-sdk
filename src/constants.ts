4c13c1b15210b322baa9de86c905a440a46aa5e8
A.L <xenide@users.noreply.github.com>
2023-06-15 15:30:01 +0200

    fix: change router and factory addresses

--- src/constants.ts
+++ src/constants.ts
@@ -2,12 +2,15 @@ import JSBI from 'jsbi'
 
 // TODO: update this with the standardized CREATE2 address that will be the same across all chains
 // Currently it only works for the wallet with the custom mnemonic given to anvil
-export const FACTORY_ADDRESS = '0xCae997a6f253814441B878868fd6DBB32a52816f'
 
-// This address is the product of using create2 with testnet WAVAX's contract address
+// this is deployed using the ReservoirDeployer
+export const MAINNET_FACTORY_ADDRESS = '0x47e537e1452dbc9c3ee8f1420e5aaf22111d3547'
+// this is deployed manually as it wasn't necessary to use the ReservoirDeployer at that time
+export const TESTNET_FACTORY_ADDRESS = '0xCae997a6f253814441B878868fd6DBB32a52816f'
+
+
 export const TESTNET_ROUTER_ADDRESS = '0xd627FdC984a249E9b5F2df263A37368f4e459726'
-// This address is the product of using create2 with mainnet WAVAX's contract address
-export const MAINNET_ROUTER_ADDRESS = '0x7f05c63dc7ca3f99f2d3409f0017c28058c42b27'
+export const MAINNET_ROUTER_ADDRESS = '0x67bc78378723acd8876FCA16d9c6C24ff79acb8e'
 export const ROUTER_ADDRESS = TESTNET_ROUTER_ADDRESS
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 export const FEE_ACCURACY = JSBI.BigInt(1_000_000) // 100%
